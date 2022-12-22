2a3da8885adb33a28589cf72c503ec4d59eed7bc
Matheus Cardoso <matheus@cardo.so>
2022-12-22 19:25:33 -0300

    fix(script): :adhesive_bandage: Add double quote to prevent globbing and word splitting. - shellcheck(SC2086)

--- create-manifest.sh
+++ create-manifest.sh
@@ -8,7 +8,7 @@ echo "Creating a new manifest for Spin plugin ${NAME}."
 
 PLUGIN_DIR="${SCRIPT_DIR}/manifests/${NAME}"
 EXAMPLE_PLUGIN="${SCRIPT_DIR}/manifests/example/example.json"
-mkdir $PLUGIN_DIR
+mkdir "$PLUGIN_DIR"
 PLUGIN_MANIFEST="${PLUGIN_DIR}/${NAME}.json"
-echo "" > $PLUGIN_MANIFEST
-sed "s/example/${NAME}/g" $EXAMPLE_PLUGIN > $PLUGIN_MANIFEST
+echo "" > "$PLUGIN_MANIFEST"
+sed "s/example/${NAME}/g" "$EXAMPLE_PLUGIN" > "$PLUGIN_MANIFEST"
